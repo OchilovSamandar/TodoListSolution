3c6aa3ac093c5c1dee3fc6d477ac60b19d7ef228
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-07-11 11:54:39 +0500

    program.cs classiga databasega bog'lanish uchun kod yozildi

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//connect database
+var connectionString = builder.Configuration.GetConnectionString("DeafultConnection") ?? throw new InvalidOperationException;
+builder.Services.AddDbContext<TodoContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
