23c93fd4d64aafd9f7518bf7aad0aaad8b5ab930
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-07-11 11:33:02 +0500

    ITodoRepository interface yaratildi va abstarct methodlar yozildi

---
+++ TodoList/Data/ITodoRepository.cs
@@ -0,0 +1,19 @@
+﻿using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public interface ITodoRepository
+    {
+        Task CreateTodo(Todo todo);
+
+        Task DeleteTodo(Todo todo);
+
+        Task<Todo> GetTodoById(int id);
+
+        Task<IEnumerable<Todo>> GetAllTodo();
+
+        Task UpdateTodo(Todo todo);
+
+
+    }
+}
