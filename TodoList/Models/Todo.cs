968b9b66246678273b45dbcde97773f747cd9f9a
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-07-11 11:12:43 +0500

    Todo va User classlari yaratildi

--- TodoList/Models/Todo.cs
+++ TodoList/Models/Todo.cs
@@ -1,6 +1,27 @@
-﻿namespace TodoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace TodoList.Models
 {
     public class Todo
     {
+        [Required]
+        [Key]
+        public int ToDoId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? Description { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set;}
+        public DateTime UpdatedDate { get; set;}
+
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public User? User { get; set; }
+    }
     }
 }
--- TodoList/Models/User.cs
+++ TodoList/Models/User.cs
@@ -1,6 +1,21 @@
-﻿namespace TodoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
 {
     public class User
     {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public List<Todo> TodoList { get; set; } 
     }
 }
